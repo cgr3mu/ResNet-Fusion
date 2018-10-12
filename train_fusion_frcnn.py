1c84633b1b8e15f6504b71124a94e1afd5d8ee8d
Connor <32524802+cgr3mu@users.noreply.github.com>
2018-10-11 23:05:45 -0400

    Update train_fusion_frcnn.py

--- train_fusion_frcnn.py
+++ train_fusion_frcnn.py
@@ -163,7 +163,7 @@ optimizer_classifier = Adam(lr=1e-5)
 model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)], metrics = {'rpn_out_class':'accuracy'})
 model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
 model_rpn.summary()
-raw_input("Heyyy")
+
 model_classifier.compile(optimizer=optimizer_classifier, loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
 model_all.compile(optimizer='sgd', loss='mae')
 epoch_length = 1000
